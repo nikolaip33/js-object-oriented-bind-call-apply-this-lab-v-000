992f9e2696c96f03e697de7bf28a719fd6b8e3e0
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-04 19:19:34 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,7 @@ const setThisWithCall = function(fn, thisValue, arg) {
 const setThisWithApply = function(fn, thisValue, arg) {
   return fn.apply(thisValue, arg)
 }
+
+const returnNewFunctionOf = function(functionToBeCopied, thisValue) {
+  
+}
