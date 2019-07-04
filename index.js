31f3dec4a09bee7c2939d16a58482540f39ce194
Nikolai Pawlowski <nikolaip@gmail.com>
2019-07-04 19:04:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -6,3 +6,7 @@ const justInvoke = function(fn) {
 const setThisWithCall = function(fn, thisValue, arg) {
   return fn.call(thisValue, arg)
 }
+
+const setThisWithApply = function(fn, thisValue, arg) {
+  return fn.apply(thisValue, arg)
+}
